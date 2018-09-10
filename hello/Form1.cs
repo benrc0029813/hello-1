2fbc03011208e31ef0b382020bd429d44caffba7
Henry Winckles <rc0032427@student.riversidecollege.ac.uk>
2018-09-10 15:33:18 +0100

    broken

--- hello/Form1.cs
+++ hello/Form1.cs
@@ -18,8 +18,8 @@ namespace hello
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Hello");
-        }
-    }
-}
+        (
+            Messagebox.Slow("Hello"),
+        )
+    )
+)
